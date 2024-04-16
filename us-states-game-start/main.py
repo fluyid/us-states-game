a936450d225b9f2ee70ad8aa0ab969dce6dc28df
Kailash Nath <kailashnathashok@gmail.com>
2024-04-16 16:09:35 +0300

    designed the back-end

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -1,4 +1,5 @@
 import turtle
+import pandas
 
 screen = turtle.Screen()
 screen.title("U.S. States Game")
@@ -7,4 +8,23 @@ screen.addshape(image)
 
 turtle.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name?")
+data = pandas.read_csv("50_states.csv")
+list_of_states = data.state.to_list()
+# print(list_of_states)
+guessed_states = []
+
+while len(guessed_states) < 50:
+
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="What's another state's name?").title()
+
+    if answer_state in list_of_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
+screen.exitonclick()
