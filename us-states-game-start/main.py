aece54fa14a31cddf49131e442c70ec5b834b337
Kailash Nath <kailashnathashok@gmail.com>
2024-04-16 16:17:47 +0300

    used a break to exit the loop and add the unguessed states to a new list and converted that to a csv

--- us-states-game-start/main.py
+++ us-states-game-start/main.py
@@ -18,6 +18,16 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
 
+    if answer_state == "Exit":
+        missing_states = []
+        for state in list_of_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        print(f"{len(missing_states)} states are missing. These are the missing states:")
+        print(missing_states)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
     if answer_state in list_of_states:
         guessed_states.append(answer_state)
         t = turtle.Turtle()
@@ -26,5 +36,3 @@ while len(guessed_states) < 50:
         state_data = data[data.state == answer_state]
         t.goto(int(state_data.x), int(state_data.y))
         t.write(state_data.state.item())
-
-screen.exitonclick()
